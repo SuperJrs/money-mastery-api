f4109cd500cbf130867a49c5e2e891393f845386
LamequeFernandes <lamequesao@gmail.com>
2023-02-25 11:13:36 -0300

    feat: congifuracoes para conexao com o db. #2

---
+++ core/deps.py
@@ -0,0 +1,12 @@
+from .database import Database
+
+
+database = Database()
+
+
+def get_session():
+    session = database.session()
+    try:
+        yield session
+    finally:
+        session.close()
